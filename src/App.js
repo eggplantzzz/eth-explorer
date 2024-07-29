32487677a8f2fba3aea2e7867d782f3fade848b9
eggplantzzz <tmfeickert@gmail.com>
2024-07-29 15:52:00 -0400

    add web3 and some basic functionality for displaying basic chain data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react"
+import { Web3 } from "web3";
+
+const web3 = new Web3('https://eth.llamarpc.com');
 
 function App() {
+  const [blockNumber, setBlockNumber] = useState();
+  const [chainId, setChainId] = useState();
+
+  useEffect(() => {
+    async function getBlockData () {
+      setBlockNumber(await web3.eth.getBlockNumber());
+      setChainId(await web3.eth.getChainId());
+    };
+
+    if (!blockNumber || !chainId) {
+      getBlockData();
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          blockNumber && chainId ?
+          <p>
+            The block number is {blockNumber.toString()}.
+            The chainId is {chainId.toString()}.
+          </p>
+          :
+          null
+        }
         <a
           className="App-link"
           href="https://reactjs.org"
