2ad76955ba067a42e63a1e094a22172e128d40c8
eggplantzzz <tmfeickert@gmail.com>
2024-07-30 17:22:07 -0400

    remove react link

--- src/App.js
+++ src/App.js
@@ -47,14 +47,6 @@ function App() {
           :
           null
         }
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
