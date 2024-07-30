f79b643435cecdd7187053759631dfc535afd6c0
eggplantzzz <tmfeickert@gmail.com>
2024-07-30 14:53:15 -0400

    get a simple server running and create a POC for requests from the ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { useEffect, useState } from "react"
 import { Web3 } from "web3";
+import axios from "axios";
 
-const web3 = new Web3('https://eth.llamarpc.com');
+const web3 = new Web3("https://eth.llamarpc.com");
 
 function App() {
   const [block, setBlock] = useState();
 
   useEffect(() => {
     async function getBlockData () {
-      setBlock(await web3.eth.getBlock());
+      try {
+        const response = await axios.get("http://localhost:3001");
+        console.log("the response from the server is -- %o", response);
+        setBlock(await web3.eth.getBlock());
+      } catch (error) {
+        console.log("there was an error while fetching data -- %o", error);
+      }
     };
 
     if (!block) {
       getBlockData();
     }
-  }, []);
+  }, [block]);
 
   return (
     <div className="App">
@@ -24,14 +31,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         {
           block ?
-          <l>
+          <ul>
             <li>Number - {block.number.toString()}.</li>
             <li>Timestamp - {block.timestamp.toString()}</li>
             <li>Size - {block.size.toString()}</li>
             <li>Gas Limit - {block.gasLimit.toString()}</li>
             <li>Hash - {block.hash}</li>
             <li>Nonce - {block.nonce.toString()}</li>
-          </l>
+          </ul>
           :
           null
         }
